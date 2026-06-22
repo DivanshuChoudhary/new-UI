f75f2b8b642fd02f023d58f975fa79fd8fe8dcb2
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-22 13:22:44 +0530

    dark mode toggle button

--- script.js
+++ script.js
@@ -42,3 +42,18 @@ function getWeather() {
         document.getElementById("wind").innerText = wind;
     }
 }
+
+let btn = document.getElementById("themeToggle");
+
+btn.onclick = function(){
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        btn.innerText = "☀ Light Mode";
+    }
+    else{
+        btn.innerText = "🌙 Dark Mode";
+    }
+
+}
